13c98cb3998943a4adc5455b10fe20f18da6dd21
ChxDesk <chackop@gmail.com>
2021-01-01 02:24:17 +0000

    Added simple filtering

--- src/components/vis/GraphVis.js
+++ src/components/vis/GraphVis.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Graph from "react-graph-vis";
 
-const graph = {
+const initgraph = {
   nodes: [
     { id: 1, label: "Node 1", title: "node 1 tootip text", color: "#e04141" },
     { id: 2, label: "Node 2", title: "node 2 tootip text", color: "#e09c41" },
@@ -10,15 +10,54 @@ const graph = {
     { id: 5, label: "Node 5", title: "node 5 tootip text", color: "#41e0c9" },
   ],
   edges: [
-    { from: 1, to: 2 },
-    { from: 1, to: 3 },
-    { from: 2, to: 3 },
-    { from: 2, to: 4 },
-    { from: 2, to: 5 },
+    {
+      from: 1,
+      to: 2,
+      label: "1to2",
+      relation: "active",
+      arrows: "to, from",
+      color: { color: "green" },
+    },
+    {
+      from: 1,
+      to: 3,
+      label: "1to3",
+      relation: "inactive",
+      arrows: "to, from",
+      color: { color: "red" },
+    },
+    {
+      from: 2,
+      to: 3,
+      label: "2to3",
+      relation: "active",
+      arrows: "to, from",
+      color: { color: "green" },
+    },
+    {
+      from: 2,
+      to: 4,
+      label: "2to4",
+      relation: "inactive",
+      arrows: "to, from",
+      color: { color: "red" },
+    },
+    {
+      from: 2,
+      to: 5,
+      label: "2to5",
+      relation: "inactive",
+      arrows: "to, from",
+      color: { color: "red" },
+    },
   ],
 };
 
 const options = {
+  interaction: { hover: true },
+  manipulation: {
+    enabled: true,
+  },
   layout: {
     hierarchical: true,
   },
@@ -29,22 +68,43 @@ const options = {
   width: "500px",
 };
 
-const events = {
-  select: function (event) {
-    let { nodes, edges } = event;
-  },
-};
-
 const GraphVis = () => {
+  const [selectnode, setselectnode] = useState("Node Selection");
+  const [edgeactive, setedgeactive] = useState(false);
+  const [graphdata, setgraphdata] = useState(initgraph);
+  let selectedNodeTitle = `Selected node ${selectnode}`;
+
+  const events = {
+    select: function (event) {
+      setselectnode(event.nodes[0]);
+    },
+    showPopup: (evt) => console.log("popupshow", evt),
+  };
+
+  const handleActive = (evt) => {
+    let newEdges = initgraph.edges.filter((item) => item.relation === "active");
+    console.log("newGraphData", newEdges);
+    console.log("initgraph", initgraph);
+    let newGraphData = { ...initgraph, edges: newEdges };
+    setgraphdata(newGraphData);
+    setedgeactive(!edgeactive);
+  };
+
   return (
-    <Graph
-      graph={graph}
-      options={options}
-      events={events}
-      //   getNetwork={(network) => {
-      //     //  if you want access to vis.js network api you can set the state in a parent component using this property
-      //   }}
-    />
+    <>
+      <form>
+        {selectedNodeTitle && <h4>{selectedNodeTitle}</h4>}
+        <label>Active connections</label>
+        <input type="checkbox" onChange={handleActive} value={edgeactive} />
+      </form>
+      <Graph
+        graph={graphdata}
+        // nodes={graph.nodes}
+        // edges={graph.edges}
+        options={options}
+        events={events}
+      />
+    </>
   );
 };
 
